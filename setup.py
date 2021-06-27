8c0808c973b482fc31fae5ad6cd00268344b1ff4
TanyaradzwaGozhora <51749063+TanyaradzwaGozhora@users.noreply.github.com>
2021-06-27 21:36:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(name='gozhora_distributions',
-      version='0.2',
+      version='0.3',
       description='Some Stats distributions',
       packages=['gozhora_distributions'],
       author="Tanyaradzwa Gozhora",
       author_email="tanyaradzwagozhora@gmail.com",
+      long_description_content_type="text/markdown",
       zip_safe=False)
