b83bf1812d299739565711a1c94fb3409b8defb6
TanyaradzwaGozhora <51749063+TanyaradzwaGozhora@users.noreply.github.com>
2021-07-21 13:32:42 +0200

    More info in setup

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
-from setuptools import setup
+from setuptools import _install_setup_requires, setup
 def readme():
     with open('README.md') as f:
         return f.read()
 
 
 setup(name='stats265',
-    version='0.72',
+    version='0.75',
     description='Statistics distributions from 265',
     packages=['stats265'],
+    license="MIT",
     author="Tanyaradzwa Gozhora",
     author_email="tanyaradzwagozhora@gmail.com",
     long_description=readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/TanyaradzwaGozhora/stats265",
+    download_url ="https://github.com/TanyaradzwaGozhora/stats265/releases/tag/v0.75",
+    keywords = ["STATS", "STATISTICS", "DISTRIBUTION"],
+    _install_setup_requires=[
+        "math",
+        "matplotlib"
+         ],
     project_urls={
     "Stats265-distributions": "https://github.com/TanyaradzwaGozhora/stats265",
     },
